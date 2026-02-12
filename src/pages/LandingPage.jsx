b835a1ace0a0cf043b68cc0acb40f75d3d7f10b8
KMF45 <kmf45.ai@gmail.com>
2026-02-12 22:57:17 +0200

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,1 +1,19 @@
+import Navbar from '../components/landing/Navbar';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import Download from '../components/Download';
+import Footer from '../components/Footer';
 
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Download />
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
