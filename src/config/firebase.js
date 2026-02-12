35cc264641dd3528054493abfa4e040ae94fb39d
KMF45 <kmf45.ai@gmail.com>
2026-02-12 22:54:40 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCvwcJ8QrzKb_k1iDwZLcudVmRGj9zjgs8",
+  authDomain: "kmf-trading-journal.firebaseapp.com",
+  databaseURL: "https://kmf-trading-journal-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kmf-trading-journal",
+  storageBucket: "kmf-trading-journal.firebasestorage.app",
+  messagingSenderId: "62737960002",
+  appId: "1:62737960002:web:81089a0c4d68e07ee571db",
+  measurementId: "G-MT8JT23VC1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export default app;
