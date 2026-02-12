06149191814edf90f3726f8b31d9c53206f0322f
KMF45 <kmf45.ai@gmail.com>
2026-02-12 23:25:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   }
 })
